5b4df7ddaaed4aae1fa9c84e80918c3d0576fcaf
markd23-sharpaxe <mark.durno@yahoo.co.uk>
2025-05-17 22:16:57 +0100

    Add files via upload

---
+++ client/src/types/react-simple-maps.d.ts
@@ -0,0 +1,7 @@
+// This module is no longer used in the project
+// We've replaced react-simple-maps with a custom implementation
+// that doesn't require any external dependencies
+
+declare module 'react-simple-maps' {
+  // Empty declaration to satisfy TypeScript
+}
